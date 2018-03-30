0c3ed75df34fa8dad03a8fab70ad3b25a1dd3a0a
sabbiu <070bct531@ioe.edu.np>
2018-03-30 18:49:01 +0545

    travis.yml added

--- test/specs/test-title.js
+++ test/specs/test-title.js
@@ -2,6 +2,6 @@ describe('Test to check for title', function() {
   it('should check for title google', function() {
   return browser
   .url('http://google.com')
-   .getTitle().should.eventually.be.equal('Google');
+   .getTitle().should.equal('Google');
   });
  });
