34fd7d7e24df02a44b889e8d136a7942c0ca0ddf
sabbiu <070bct531@ioe.edu.np>
2018-03-30 21:47:33 +0545

    updated travisci for headless

--- wdio.conf.js
+++ wdio.conf.js
@@ -44,7 +44,11 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'chrome'
+        browserName: 'chrome',
+
+        chromeOptions: {
+            args: ['headless', 'disable-gpu']
+        }
     }],
     //
     // ===================
